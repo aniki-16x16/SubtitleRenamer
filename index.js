e6a8d2cab5db5facc5dcb3b7501151a3f7666d0c
aniki-16x16 <aniki_16x16@outlook.com>
2021-02-18 14:10:14 +0800

    refactor: 删去不必要的async/await

--- index.js
+++ index.js
@@ -9,45 +9,46 @@ const rename = promisify(fs.rename);
 program
   .version("0.0.1")
   .requiredOption("-p, --path <path>", "目标文件夹路径")
-  .option('--vr <RegExp>', '视频文件的匹配正则')
-  .option('--sr <RegExp>', '字幕文件的匹配正则')
+  .option("--vr <RegExp>", "视频文件的匹配正则")
+  .option("--sr <RegExp>", "字幕文件的匹配正则")
   .parse();
 
 const options = program.opts();
-async function main() {
-  try {
-    const files = fs
-      .readdirSync(options.path)
-      .filter((file) => /^.+\..+$/.test(file));
-    const videoRegExp = options.vr ? new RegExp(options.vr) : /(mp4|mkv)$/;
-    const subtitleRegExp = options.sr ? new RegExp(options.sr) : /(srt|ass)$/;
-    const videos = files.filter((file) => videoRegExp.test(file));
-    const subtitles = files.filter((file) => subtitleRegExp.test(file));
+try {
+  const files = fs
+    .readdirSync(options.path)
+    .filter((file) => /^.+\..+$/.test(file));
+  const videoRegExp = options.vr ? new RegExp(options.vr) : /(mp4|mkv)$/;
+  const subtitleRegExp = options.sr ? new RegExp(options.sr) : /(srt|ass)$/;
+  const videos = files.filter((file) => videoRegExp.test(file));
+  const subtitles = files.filter((file) => subtitleRegExp.test(file));
 
-    if (videos.length === subtitles.length) {
-      try {
-        await Promise.all(
-          subtitles.map((file, index) =>
-            rename(
-              resolve(options.path, file),
-              resolve(
-                options.path,
-                `${videos[index].split(".")[0]}.${file.split(".").pop()}`
-              )
-            )
+  if (videos.length === subtitles.length) {
+    Promise.all(
+      subtitles.map((file, index) =>
+        rename(
+          resolve(options.path, file),
+          resolve(
+            options.path,
+            `${videos[index].split(".")[0]}.${file.split(".").pop()}`
           )
-        );
+        )
+      )
+    )
+      .then(() => {
         console.log(chalk.greenBright("字幕重命名完成"));
-      } catch (e) {
+      })
+      .catch(() => {
         console.error(chalk.redBright("ERROR: 重命名时出现错误"));
-      }
-    } else {
-      console.log(chalk.yellowBright('视频数量与字幕数量不一致, 请使用--vr或--sr参数进行过滤'));
-      return;
-    }
-
-  } catch (e) {
-    console.error(chalk.redBright("ERROR: 无法打开文件夹，请检查路径"));
+      });
+  } else {
+    console.log(
+      chalk.yellowBright(
+        "视频数量与字幕数量不一致, 请使用--vr或--sr参数进行过滤"
+      )
+    );
+    return;
   }
+} catch (e) {
+  console.error(chalk.redBright("ERROR: 无法打开文件夹，请检查路径"));
 }
-main();
